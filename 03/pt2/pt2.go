65bda90a259e91738e593ef4cdaa6bc919ea3b5d
FacuBar <efacubar@gmail.com>
2021-12-04 00:06:44 -0300

    improving performance

--- 03/pt2/pt2.go
+++ 03/pt2/pt2.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	f, err := os.Open("03/data")
+	f, err := os.Open("../data")
 	if err != nil {
 		panic("couldnt open file")
 	}
@@ -41,17 +41,44 @@ func getRating(binaries []string, isOxygen bool, response chan int64) {
 	var filterBy byte
 	var val int64
 
-	for i := 0; i < len(binaries[0]); i++ {
+	n := len(binaries[0])
+	for i := -1; i < n; i++ {
 		counter = 0
 
-		for _, bits = range binaries {
-			switch bits[i] {
-			case '1':
-				counter++
-				break
-			case '0':
-				counter--
+		newBinaries := make([]string, 0)
+
+		// At the begining just the most common binary is identified
+		if i == -1 {
+			for _, bits = range binaries {
+				switch bits[i+1] {
+				case '1':
+					counter++
+				case '0':
+					counter--
+				}
 			}
+			// At the end t
+		} else if i == n-1 {
+			for _, bits = range binaries {
+				if bits[i] == filterBy {
+					newBinaries = append(newBinaries, bits)
+				}
+			}
+			binaries = newBinaries
+		} else {
+			for _, bits = range binaries {
+				if bits[i] == filterBy {
+					newBinaries = append(newBinaries, bits)
+
+					switch bits[i+1] {
+					case '1':
+						counter++
+					case '0':
+						counter--
+					}
+				}
+			}
+			binaries = newBinaries
 		}
 
 		if isOxygen {
@@ -68,8 +95,6 @@ func getRating(binaries []string, isOxygen bool, response chan int64) {
 			}
 		}
 
-		binaries = filter(binaries, filterBy, i)
-
 		if len(binaries) == 1 {
 			val, _ = strconv.ParseInt(binaries[0], 2, 64)
 			response <- val
@@ -80,15 +105,15 @@ func getRating(binaries []string, isOxygen bool, response chan int64) {
 	response <- val
 }
 
-func filter(binaries []string, filterBy byte, i int) []string {
-	newBinaries := make([]string, 0)
-	var bits string
+// func filter(binaries []string, filterBy byte, i int) []string {
+// 	newBinaries := make([]string, 0)
+// 	var bits string
 
-	for _, bits = range binaries {
-		if bits[i] == filterBy {
-			newBinaries = append(newBinaries, bits)
-		}
-	}
+// 	for _, bits = range binaries {
+// 		if bits[i] == filterBy {
+// 			newBinaries = append(newBinaries, bits)
+// 		}
+// 	}
 
-	return newBinaries
-}
+// 	return newBinaries
+// }
